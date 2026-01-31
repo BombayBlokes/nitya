235a1214aab5409aa426f04820182ad65f5d5a9f
Aryan Kuril <aryan@bombayblokes.com>
2026-01-31 12:07:35 +0530

    error sloved

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const baseUrl = "www.nityatraining.com";
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
+<url>
+  <loc>https://www.nityatraining.com/</loc>
+  <lastmod>2026-01-29T04:53:21+00:00</lastmod>
+</url>
+
+
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: { "Content-Type": "application/xml" },
+  });
+}
